dcaf90f08b93c0f4fd7dd49de2c11b1fbfadc912
Christina Yang <ychristina011@gmail.com>
2024-06-20 19:19:43 -0700

    remove www.

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -17,7 +17,7 @@ const projectsData: ProjectsData = {
         parsing via OpenAI API, a 2-way Google calendar sync, task organizing views, and productivity
         tracking.`,
       imgSrc: '/static/images/planda-cover-photo.png',
-      href: 'https://www.planda.ca',
+      href: 'https://planda.ca',
     },
     {
       title: 'Celebrate This Mortal',
